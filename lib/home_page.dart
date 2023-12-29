b498271bf3799bce3cc33952932950ea84f08923
saminsohag <sssohag172@gmail.com>
2023-12-29 22:28:18 +0600

    ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -22,66 +23,161 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body: Column(
-        children: [
-          // Expanded(
-          //   child: ListView.builder(
-          //     itemCount: names.length,
-          //     itemBuilder: (cintext, index) {
-          //       return Card(
-          //         child: ListTile(title: Text(names[index])),
-          //       );
-          //     },
-          //   ),
-          // )
-          // Expanded(
-          //   child: GridView.count(
-          //     crossAxisCount: 3,
-          //     mainAxisSpacing: 5,
-          //     crossAxisSpacing: 10,
-          //     padding: const EdgeInsets.all(10),
-          //     children: [
-          //       Container(
-          //         color: Colors.red,
-          //       ),
-          //       Container(
-          //         color: Colors.blue,
-          //       ),
-          //       Container(
-          //         color: Colors.green,
-          //       ),
-          //       Container(
-          //         color: Colors.yellow,
-          //       ),
-          //       Container(
-          //         color: Colors.black,
-          //       ),
-          //       Container(
-          //         color: Colors.pink,
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          Expanded(
-            child: GridView.builder(
-              itemCount: colors.length,
-              padding: const EdgeInsets.all(10),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
+        leading: const Icon(Icons.window),
+        title: const Column(
+          children: [
+            Text(
+              "Hello sohag",
+              style: TextStyle(
+                fontSize: 16,
               ),
-              itemBuilder: (context, index) {
-                //
-                return Container(
-                  color: colors[index],
-                );
-              },
+            ),
+            Text(
+              "New york",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        actions: const [
+          Icon(Icons.account_circle),
+          SizedBox(
+            width: 10,
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: SearchBar(
+              leading: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Icon(
+                  Icons.search,
+                  size: 28,
+                ),
+              ),
+            ),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CustomIconButton(icon: Icons.calendar_month),
+              CustomIconButton(
+                icon: Icons.message,
+              ),
+              CustomIconButton(icon: Icons.location_on),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Featured",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                  ),
+                ),
+                Text("view all"),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) => const CustomCard(),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Bast seller",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                  ),
+                ),
+                Text("view all"),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) => const CustomCard(),
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "home"),
+          NavigationDestination(icon: Icon(Icons.shop), label: "home"),
+          NavigationDestination(icon: Icon(Icons.message), label: "home"),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomIconButton extends StatelessWidget {
+  const CustomIconButton({super.key, required this.icon});
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton.filledTonal(
+      style: IconButton.styleFrom(
+        padding: const EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18),
+        ),
+      ),
+      onPressed: () {},
+      icon: Icon(
+        icon,
+        size: 35,
+      ),
+    );
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150,
+      height: 80,
+      child: Card(
+        child: Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: IconButton.filledTonal(
+              onPressed: () {},
+              icon: const Icon(
+                CupertinoIcons.heart_fill,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      home: const HomePage(),
     );
   }
 }
